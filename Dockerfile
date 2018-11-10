9650c5d7ca0e2bd38ff327b83d8dc92a1811c7cd
Walter Raul Canchan Hermoza <wcanchan@gmail.com>
2018-11-10 08:27:48 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:6.9
+EXPOSE 3000
+
+WORKDIR /app
+
+ADD package.json /app/
+
+RUN npm install
+
+ADD . /app
+
+CMD ["node", "server"]
---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app');
+
+http.createServer(app).listen(3000);
+
+console.log('ok walter');
+
+
