fd630f2d615157f9fdb319b7612d70fa09486358
Walter Raul Canchan Hermoza <wcanchan@gmail.com>
2018-11-10 20:21:17 -0500

    agregando main

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
-npm install -g now
+sudo npm install -g --unsafe-perm now
 echo "deploying..."
-URL = $(now --docker -t $NOW_TOKEN)
+URL=$(now --docker --public -t $NOW_TOKEN)
 echo "running acceptance on $URL"
-curl --silent -L $URL
+curl --silent -L $URL 
