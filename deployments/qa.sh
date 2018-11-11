67098a18d9a8a67084314e2239cc72ab7d2fee2d
Walter Raul Canchan Hermoza <wcanchan@gmail.com>
2018-11-10 20:33:39 -0500

    agregando main

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
-npm install -g now
+sudo npm install -g now
 echo "deploying..."
 URL=$(now --docker --public -t $NOW_TOKEN)
 echo "running acceptance on $URL"
-curl --silent -L $URL 
+curl --silent -L $URL
