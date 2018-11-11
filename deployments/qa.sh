27127ff6ae8b4c4cc583ad7c4a2b08f670e9a3f7
Walter Raul Canchan Hermoza <wcanchan@gmail.com>
2018-11-10 21:36:48 -0500

    agregando main

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,4 +1,5 @@
-sudo npm i -g --unsafe-perm now
+sudo npm install -g npmlog
+sudo npm install -g --unsafe-perm now
 echo "deploying..."
 URL=$(now --docker --public -t $NOW_TOKEN)
 echo "running acceptance on $URL"
